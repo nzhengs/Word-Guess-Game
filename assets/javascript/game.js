38bb9b7763f48f9cdb989e6ea6dbac0dbfa867cf
Anjana Hengaju <an.hengaju@gmail.com>
2019-02-10 19:21:22 -0600

    Add display logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var randomWords = ["apple", "banana", "cat", "mango"];
+var randomWords = ["apicort", "brinjal", "cat", "mango", "banana"];
 
 function getRandomInt(max) {
   var random = Math.random();
@@ -6,24 +6,69 @@ function getRandomInt(max) {
   var randomInt = Math.floor(randomNumber);
   return randomInt;
 }
-function getRandomSelected(){
-    var randomIndex = getRandomInt(randomWords.length);
-    var randomSelected = randomWords[randomIndex];
-    return randomSelected;
+function getRandomSelected() {
+  var randomIndex = getRandomInt(randomWords.length);
+  var randomSelected = randomWords[randomIndex];
+  return randomSelected;
 }
- var dash = "_ ";
- var guessedWord = getRandomSelected()
- var totalDashes = dash.repeat(guessedWord.length);
- document.onkeyup = function (event) {
-    var keyPress = event.key;
-    console.log(keyPress)
-    for(var i=0;i<guessedWord.length;i++){
-    if (keyPress === guessedWord[i]) {
-        ;
-        
-    
- }
+function getCharIndices(str, char) {
+  var indices = [];
+  for (var i = 0; i < str.length; i++) {
+    var c = str.charAt(i);
+    if (char.toLowerCase() === c.toLowerCase()) {
+      indices.push(i);
+    }
+  }
+  return indices;
 }
 
-// console.log(totalDashes);
-// console.log(guessedWord);
+function displayWithDashes(array) {
+  var dashed = array.map(function(a) {
+    return (a === "") ? "_" : a;
+  });
+
+  var dashes = dashed.join(" ");
+  $("#dashes").text(dashes);
+}
+
+var guessedWord = getRandomSelected();
+var remainingAttempts = guessedWord.length + 6;
+var correctAttempt = new Array(guessedWord.length);
+correctAttempt.fill("");
+console.log(guessedWord);
+displayWithDashes(correctAttempt);
+
+var incorrectAttempt = [];
+var allKeyPressed = [];
+document.onkeyup = function(event) {
+  var keyPress = event.key;
+  keyPress = keyPress.toLowerCase();
+  if (allKeyPressed.indexOf(keyPress) >= 0) {
+    return;
+  }
+  allKeyPressed.push(keyPress);
+
+  var index = guessedWord.indexOf(keyPress);
+  if (index >= 0) {
+    var indices = getCharIndices(guessedWord, keyPress);
+    indices.forEach(function(i) {
+      correctAttempt[i] = keyPress;
+    });
+    displayWithDashes(correctAttempt);
+    console.log(correctAttempt);
+    if (guessedWord === correctAttempt.join("")) {
+      console.log("You Won");
+    }
+  } else {
+    if (incorrectAttempt === keyPress) {
+    }
+    incorrectAttempt.push(keyPress);
+    $("#falseAttempts").text(incorrectAttempt);
+  }
+
+  remainingAttempts--;
+
+  if (remainingAttempts === 0) {
+    console.log("Try Again");
+  }
+};
